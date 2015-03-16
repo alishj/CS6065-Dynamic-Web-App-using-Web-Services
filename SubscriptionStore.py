e885c06472cd477059e70c7703d1fa382b19b7dc
Anthony Kleiser <kleiseaj@mail.uc.edu>
2015-03-16 17:03:17 -0400

    Worked in unsubscribe feature.

--- SubscriptionStore.py
+++ SubscriptionStore.py
@@ -19,26 +19,21 @@ class SubscriptionGroupModel(ndb.Model):
 
 class Unsubscribe(webapp2.RequestHandler):
     def get(self):
-        if True:
-            logging.debug("This functionality is not supported yet")
-            self.redirect("/")
+        user = users.get_current_user()
+        if user and user.user_id() is not None:
+            query = SubscriptionGroupModel.query(
+                SubscriptionGroupModel.subscriber_id == user.user_id()
+            )
+            current_subscription_group = query.get()
+            if current_subscription_group is not None:
+                current_subscription_group.key.delete()
+            self.redirect('/')
+        elif not user:
+            self.response.headers['Content-Type'] = 'text/html'
+            self.response.write('<a href="%s">Sign in</a>' % users.create_login_url('/'))
         else:
-            user = users.get_current_user()
-            if user and user.user_id() is not None:
-                query = SubscriptionGroupModel.query(
-                    SubscriptionGroupModel.subscriber_id == user.user_id()
-                )
-                current_subscription_group = query.get()
-                if current_subscription_group is not None:
-                    current_subscription_group.delete()
-                    current_subscription_group.put()
-                self.redirect('/')
-            elif not user:
-                self.response.headers['Content-Type'] = 'text/html'
-                self.response.write('<a href="%s">Sign in</a>' % users.create_login_url('/'))
-            else:
-                self.response.headers['Content-Type'] = 'text/html'
-                self.response.write('Please <a href=%s>sign in</a> with a Google account' % users.create_login_url('/'))
+            self.response.headers['Content-Type'] = 'text/html'
+            self.response.write('Please <a href=%s>sign in</a> with a Google account' % users.create_login_url('/'))
 
 
 class Subscribe(webapp2.RequestHandler):
