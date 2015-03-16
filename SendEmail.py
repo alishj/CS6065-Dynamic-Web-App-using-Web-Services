e5a17fbac676bc5033e7145178c8d80313b3cf2f
schatzsr <schatzsr@ucmail.uc.edu>
2015-03-16 16:27:50 -0400

    Finished basic send-email functionality

--- SendEmail.py
+++ SendEmail.py
@@ -8,7 +8,7 @@ from SubscriptionStore import SubscriptionGroupModel
 APP_EMAIL = 'anthonykleiser@gmail.com'
 DAYS = {
     0: 'monday_content',
-    1: 'monday_content',
+    1: 'tuesday_content',
     2: 'wednesday_content',
     3: 'thursday_content',
     4: 'friday_content',
@@ -17,6 +17,11 @@ DAYS = {
 }
 
 class SendEmail(webapp2.RedirectHandler):
+    """
+    Retrieves the current user and queries the Subscription datastore based on user_id.
+    An email is sent to the user containing the urls they are subscribed to for the current day.
+    """
+
     def get(self):
         user = users.get_current_user()
         day = DAYS[datetime.today().weekday()]
@@ -26,17 +31,14 @@ class SendEmail(webapp2.RedirectHandler):
             query = SubscriptionGroupModel.query(
                 SubscriptionGroupModel.subscriber_id == user.user_id())
             current_subscription_group = query.get()
+
+            # Create string to use in eval
             query_day = 'current_subscription_group.' + day
-            daily_links = eval(query_day)
-            
+            # Queries datastore for the current day's links
+            daily_links = str(eval(query_day)).replace(';', '\n')
 
-
-
-
-            message = ''
-
-            template_values = {
-                'message': message
-            }
+            subject = 'Your Daily Subscription'
+            body = 'Your content for the day is...\n' + daily_links
+            mail.send_mail(APP_EMAIL, user_email, subject, body)
 
         self.redirect('/')
